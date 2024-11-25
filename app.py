b2d0e9d053f01aed413bf18e0c860e2cf6a664d6
Muneeb Ul Hassan <133370001+MuneebUH@users.noreply.github.com>
2024-11-25 22:01:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,6 @@ async def predict_route(text):
     except Exception as e:
         raise e
     
-
+# to run on EC2, ensure following port settings
 if __name__=="__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
